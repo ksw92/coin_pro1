9e0eb1ad57b20f9f202de89a89312b825643b3d4
ksw92 <106147660+ksw92@users.noreply.github.com>
2022-06-08 20:08:47 +0900

    Update ksw_coin2.py

--- ksw_coin2.py
+++ ksw_coin2.py
@@ -4,9 +4,9 @@ import datetime
 import requests
 
 ################################################
-access = ""
-secret = ""
-stock = ""
+access = "5I9JQCQ21obbyno3631A2xlisGjCVgzxLw8csbDb"
+secret = "qJytN9DZfS9ruxkBDyllUS7hmYBM7RvaFy9DRWS8"
+stock = "xoxb-3566443758993-3574327487846-Te6mTzErJS6YnjW2rPjVK2dg"
 
 
 ################################################
@@ -26,10 +26,16 @@ def get_target_price(ticker, k):
     target_p = df.iloc[0]['close'] + check_p
     return check_p, target_p
 
+def get_targetB_price(ticker, k):
+    """변동성 돌파 전략으로 매도 목표가 조회"""
+    df = pyupbit.get_ohlcv(ticker, interval="day", count=2)
+    check_p = (df.iloc[0]['high'] - df.iloc[0]['low']) * k
+    targetB_p = df.iloc[0]['close'] - check_p
+    return check_p, targetB_p
 
 def get_ma15(ticker):
     """15 이동평균선 값"""
-    conin_OHLCV = pyupbit.get_ohlcv(ticker, interval="minute30", count=15)
+    conin_OHLCV = pyupbit.get_ohlcv(ticker, interval="day", count=15)
     ma15 = conin_OHLCV['close'].rolling(15).mean().iloc[-1]
     ma15 = round(ma15, 1)
     return ma15
@@ -37,7 +43,7 @@ def get_ma15(ticker):
 
 def get_ma50(ticker):
     """50 이동평균선 값"""
-    conin_OHLCV = pyupbit.get_ohlcv(ticker, interval="minute30", count=50)
+    conin_OHLCV = pyupbit.get_ohlcv(ticker, interval="day", count=50)
     ma50 = conin_OHLCV['close'].rolling(50).mean().iloc[-1]
     ma50 = round(ma50, 1)
     return ma50
@@ -79,40 +85,42 @@ def get_balance(ticker):
 upbit = pyupbit.Upbit(access, secret)
 buy_check = 0
 sell_check = 0
+post_message(stock, "#stock", "[%s] - AUTO UPBIT START")
 ################################################
 while True:
     try:
         start_time = get_start_time("KRW-XRP")           # 시작시간
         current_time = datetime.datetime.now()           # 현시간
-        target_price = get_target_price("KRW-XRP", 0.3)  # 목표가
+        target_price = get_target_price("KRW-XRP", 0.3)  # 목표가(매수)
+        targetB_price = get_targetB_price("KRW-XRP", 0.3)# 목표가(매도)
         current_price = get_current_price("KRW-XRP")     # 현재가
         close_price = get_close_price("KRW-XRP")         # 종가
         ma15_price = get_ma15("KRW-XRP")                 # 15ma
         ma50_price = get_ma50("KRW-XRP")                 # 50ma
         korea_price = get_balance("KRW")                 # 내지갑
-        sell_price = get_balance("XRP")
+        sell_price = get_balance("XRP")                  # 내XRP
 
         if buy_check == 0:
             if ma15_price == ma50_price:
-                # print("[CHECK] ma15 = ma50")
+                print("[CHECK] ma15 = ma50")
                 post_message(stock, "#stock", "[CHECK] ma15 = ma50")
 
             if current_time > start_time:
                 print("CV-K : %s, TP : %s, CP : %s" % (target_price[0], target_price[1], current_price))
                 if target_price[1] <= current_price:
-                    # print("[TARGET-PRICE] TP : %s, CP : %s" % (target_price[1], current_price))
+                    print("[TARGET-PRICE] TP : %s, CP : %s" % (target_price[1], current_price))
                     post_message(stock, "#stock", "[TARGET-PRICE] CV-K : %s, TP : %s, CP : %s" % (
                     target_price[0], target_price[1], current_price))
                     if float(korea_price) > 5000:
                         buy_result = upbit.buy_market_order("KRW-XRP", float(korea_price) * 0.9995)
                         post_message(stock, "#stock", "[JACKPOT-BUY] : %s" % buy_result['market'])
-                        # print("[JACKPOT-BUY] : %s" % buy_result['market'])
+                        print("[JACKPOT-BUY] : %s" % buy_result['market'])
                         buy_check = 1
                     else:
-                        # print("[NO-MONEY]")
+                        print("[NO-MONEY]")
                         post_message(stock, "#stock", "[NO-MONEY]")
             else:
-                # print("[NO-TIME]")
+                print("[NO-TIME]")
                 post_message(stock, "#stock", "[NO-TIME]")
                 break
 
@@ -120,14 +128,15 @@ while True:
             if close_price[0] <= close_price[1]:
                 if sell_check == 0:
                     post_message(stock, "#stock", "[HODL]")
-                    # print("[HODL]")
+                    print("[HODL]")
                     sell_check = 1
                     pass
-            if close_price[0] > close_price[1]:
+
+            if targetB_price[1] >= current_price :
                 if float(sell_price) > 0.0008:
                     sell_result = upbit.sell_market_order("KRW-XRP", float(sell_price) * 0.9995)
                     post_message(stock, "#stock", "[JACKPOT-SELL] : %s" % sell_result['market'])
-                    # print("[JACKPOT-SELL] : %s" % sell_result['market'])
+                    print("[JACKPOT-SELL] : %s" % sell_result['market'])
                     sell_check = 0
                     buy_check = 0
                 time.sleep(3)
